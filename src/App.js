27f97ed5f234290c28663c8d25dbbd3b8e36ac2d
Gyekye <mrgyekye740@gmail.com>
2021-12-19 02:02:46 +0000

    feat: routing setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from "react"
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import * as ROUTES from './constants/Routes';
+
+const Dashboard = lazy(() => import ('./pages/Dashboard'));
+const Login = lazy(() => import ('./pages/Login'));
+const SignUp = lazy(() => import ('./pages/SignUp'));
+const Profile = lazy(() => import ('./pages/Profile'));
+const NotFound = lazy(() => import ('./pages/NotFound'));
+
 export default function App() {
   return (
-    <h1 className="text-red-500 text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Suspense
+      fallback={<p>Loading content</p>}>
+      <Routes>
+        <Route exact path={ROUTES.DASHBOARD} element={<Dashboard />} />
+        <Route exact path={ROUTES.LOGIN} element={<Login />} />
+        <Route exact path={ROUTES.SIGNUP} element={<SignUp />} />
+        <Route exact path={ROUTES.PROFILE} element={<Profile />} />
+        <Route exact path={ROUTES.NOT_FOUND} element={<NotFound />} />
+      </Routes>
+    </Suspense>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/SignUp.js
@@ -0,0 +1,7 @@
+export default function SignUp(){
+    return(
+        <div>
+            I am a signup
+        </div>
+    )
+}
